40b73ac5369b31fd8ded8fa5530edec5062cb29d
Puja Kumari <149191257+Pujakumari1202@users.noreply.github.com>
2025-02-14 19:37:51 +0530

    broadcast part is done in backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,32 +4,71 @@ import { WebSocketServer , WebSocket} from "ws";
 
 const wss=new WebSocketServer({port:8080});
 
-//global object to store all sockets 
-let allSockets={
 
-};
+interface User{
+    socket:WebSocket,
+    room:string;
+}
+
+
+//global object to store all sockets 
+let allSockets:User[]=[];
+
 
 wss.on("connection",(socket)=>{
-    allSockets.push(socket);
+    
 
 
     //This line will run for every user when connect
+    //this message is string type not json object
     socket.on("message",(message)=>{
-        console.log("message received from client #" + message.toString());
 
-        //sending message to all other clients(broadcasting)
-        for(let i=0;i<allSockets.length;i++){
-            const s=allSockets[i];
-            s.send(message.toString()+ ": sent from the server");
+        //convert the string into object
+        //@ts-ignore
+        const parsedMessage=JSON.parse(message);
+        if(parsedMessage.type=="join"){
+
+
+            console.log("user joined room"+ parsedMessage.payload.roomId)
+
+            
+            allSockets.push({
+                socket,
+                room:parsedMessage.payload.roomId
+            })
         }
+
+        // iterate over all sockets and find the socket that is in the same room
+        if(parsedMessage.type=="chat"){
+            console.log("user wants to chat")
+
+
+            // const currentUserRoom=allSockets.find((x)=>x.socket==socket);
+
+            //same
+            let currentUserRoom=null;
+            for(let i=0;i<allSockets.length;i++){
+                if(allSockets[i].socket==socket){
+                    currentUserRoom=allSockets[i].room
+                }
+            }
+
+            
+            for (let i = 0; i < allSockets.length; i++) {
+                if (allSockets[i].room == currentUserRoom) {
+                    allSockets[i].socket.send(parsedMessage.payload.message)
+                }
+            }
+        }
+
         
     });
 
 
     // when the client is disconnected it will remove the socket from the array
-    socket.on("disconnet",()=>{
-        allSockets=allSockets.filter(x=>x!=socket);
-    })
+    // socket.on("disconnet",()=>{
+    //     allSockets=allSockets.filter(x=>x!=socket);
+    // })
 
 
 })
