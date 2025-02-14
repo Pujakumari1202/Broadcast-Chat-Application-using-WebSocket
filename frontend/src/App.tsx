14aa32c98cb063872319e5d87e0900b8bc54c06c
Puja Kumari <149191257+Pujakumari1202@users.noreply.github.com>
2025-02-14 21:59:38 +0530

    commit the app.tsx file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,33 @@
+import './App.css';
+import {useEffect} from 'react';
 
-import './App.css'
+
+//websocket connection to the backend server
+
+
 
 function App() {
 
+  //on mounts it will connect to the backend server
+  useEffect(()=>{
 
-  return <div>
-    hii 
+    const ws=new WebSocket("http://localhost:3000");
+  },[]);
+
+
+  return <div className='h-screen bg-black'>
+    <div className='h-[90vh]'></div>
+    <div className='w-full bg-white flex p-4'>
+      <input type="text" className="flex-1 " ></input>
+      <button className='bg-purple-600 text-white'>
+        Send message
+      </button>
+
+    </div>
+   hii there
+  
   </div>
 }
 
 export default App
+
