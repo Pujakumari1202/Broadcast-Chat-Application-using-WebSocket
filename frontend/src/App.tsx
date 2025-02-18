a8185c54ce8719c31451848296d681e7518a3a0e
Puja Kumari <149191257+Pujakumari1202@users.noreply.github.com>
2025-02-18 23:26:26 +0530

    frontend is done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,30 +1,69 @@
 import './App.css';
 import {useEffect} from 'react';
-
-
-//websocket connection to the backend server
-
+import {useState} from 'react';
+import {useRef} from 'react';
 
 
 function App() {
+  const [messages,setMessages]=useState(["hii there","hello"]);
+  const wsRef=useRef();
+  const inputRef=useRef();
 
   //on mounts it will connect to the backend server
   useEffect(()=>{
 
-    const ws=new WebSocket("http://localhost:3000");
-  },[]);
+    const ws=new WebSocket("http://localhost:8080");
+
+    //when message came
+    ws.onmessage=(event)=>{
+      //new message(event) as well as old message(m) will be added to the messages array
+      setMessages(m=>[...m,event.data]);
+    }
+    wsRef.current=ws;
+ 
+    ws.onopen = () => {
+      ws.send(JSON.stringify({
+        type: "join",
+        payload: {
+          roomId: "red"
+        }
+      }))
+    }
+
+  return ()=>{
+    wsRef.close()
+  }
+},[]);
 
 
   return <div className='h-screen bg-black'>
-    <div className='h-[90vh]'></div>
-    <div className='w-full bg-white flex p-4'>
-      <input type="text" className="flex-1 " ></input>
-      <button className='bg-purple-600 text-white'>
+    <br /><br /><br />
+    <div className='h-[85vh]'>
+      {messages.map(message =><div className='m-8'>
+      <span className='bg-white text-black rounded p-4 '>
+        {message}
+      </span>
+      </div>)}
+    </div>
+    <div className='w-full bg-white flex '>
+      <input ref={inputRef} id="message" className="flex-1 p-4" ></input>
+      <button onClick={()=>{
+        const message=inputRef.current?.value;
+        wsRef.current.send(JSON.stringify({
+          type:"chat",
+          payload:{
+            message:message
+          }
+      
+        }))
+        console.log(message)
+
+      }} className='bg-purple-600 text-white p-4'>
         Send message
       </button>
 
     </div>
-   hii there
+  
   
   </div>
 }
